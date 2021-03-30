42ff6fddf965209b72ad1db7a37aa3bd38f3fdd8
tmartincpp <33619793+tmartincpp@users.noreply.github.com>
2021-03-30 17:20:35 +0200

    allow_user_to_enable_e2e_for_direct_messages
    Allow any user with to enable e2e if they have the appropriate right.

--- app/e2e/client/tabbar.ts
+++ app/e2e/client/tabbar.ts
@@ -11,7 +11,7 @@ addAction('e2e', ({ room }) => {
 	const e2eEnabled = useSetting('E2E_Enable');
 	const e2eReady = e2e.isReady() || room.encrypted;
 	const e2ePermission = room.t === 'd' || usePermission('toggle-room-e2e-encryption', room._id);
-	const hasPermission = usePermission('edit-room', room._id) && e2ePermission && e2eReady;
+	const hasPermission = ( usePermission('edit-room', room._id) || room.t === 'd' ) && e2ePermission && e2eReady;
 	const toggleE2E = useMethod('saveRoomSettings');
 
 	const action = useMutableCallback(() => {
